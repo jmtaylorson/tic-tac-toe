6d4db24ca30bca16a41dc8b2e45980614b401e29
James Taylorson <jmtaylorson@gmail.com>
2016-11-15 11:21:07 -0800

    i done

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,115 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var rectcolor = "yellow"
+function topleft() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("topleftrect")
+rect.setAttribute("fill", "red")
+
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+
+
+}
+function topmiddle() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("topmiddlerect")
+rect.setAttribute("fill", "red")
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+}
+function topright() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("toprightrect")
+rect.setAttribute("fill", "red")
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+}
+function midleft() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("middleleftrect")
+rect.setAttribute("fill", "red")
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+}
+function mid() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("midrect")
+rect.setAttribute("fill", "red")
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+}
+function midright() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("midrightrect")
+rect.setAttribute("fill", "red")
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+}
+function botleft() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("botleftrect")
+rect.setAttribute("fill", "red")
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+}
+function botmiddle() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("botmiddlerect")
+rect.setAttribute("fill", "red")
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+}
+function botright() {
+  var canvas = document.getElementById("game-board")
+var rect = document.getElementById("botrightrect")
+rect.setAttribute("fill", "red")
+if(rectcolor=="red") {
+  rect.setAttribute("fill", "yellow")
+  rectcolor = "yellow"
+} else {
+  rect.setAttribute("fill", "red")
+  rectcolor = "red"
+}
+}
